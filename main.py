e3d210289cbedaf345a9b0fa52726870ba549719
M-Adánez <m.adanez@opendeusto.es>
2026-04-12 19:56:59 +0200

    cambios de varaible y distintos scores

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import optuna
 import numpy as np
 import pandas as pd
@@ -24,7 +23,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, accuracy_score, balanced_accuracy_score
 
 
 # Imbalanced dataset
@@ -37,10 +36,22 @@ from sklearn.metrics import f1_score
 ### stratification
 ## Undersampling
 
-# DATA COLLECTION
-random_seed = 42
-csv_path = "./data/stroke.csv"
-df=pd.read_csv(csv_path)
+SEED = 42
+CSV_PATH = "./data/stroke.csv"
+
+#EDITABLE PARAMETERS
+TRIAL_NUM=10
+METRIC="acc" #f1, acc or b_acc
+
+IMPUTE_DATA=True
+SAMPLE_DATA=True
+SCALE_DATA=True
+
+OUTPUT_FILE = "logs_optim_strokesAML.txt"
+
+
+
+df=pd.read_csv(CSV_PATH)
 
 # DATA PREPROCESSING
 df=df.drop(columns=["id"])
@@ -58,55 +69,65 @@ y = df_dummies['stroke']
 X_np = X.values.astype(np.float32)
 y_np = y.values
 
-skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=random_seed)
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=SEED)
 splits = list(skf.split(X, y))
 
 def objective(trial):
-    # imputation
-    imputer_type = trial.suggest_categorical("imputer_type", ["simple", "knn"])
-    if imputer_type == "simple":
-        strat = trial.suggest_categorical("simp_imp_strategy", ["mean", "median", "most_frequent"])
-        imputer = SimpleImputer(strategy=strat)
-    elif imputer_type == "knn":
-        n_neighbors = trial.suggest_int("knn_input_neighs", 3, 20)
-        imputer = KNNImputer(n_neighbors=n_neighbors)
+    #Imputation
+    imputer=None
+    if IMPUTE_DATA:
+        imputer_type = trial.suggest_categorical("imputer_type", ["simple", "knn"])
+        if imputer_type == "simple":
+            strat = trial.suggest_categorical("simp_imp_strategy", ["mean", "median", "most_frequent"])
+            imputer = SimpleImputer(strategy=strat)
+        elif imputer_type == "knn":
+            n_neighbors = trial.suggest_int("knn_input_neighs", 3, 10)
+            imputer = KNNImputer(n_neighbors=n_neighbors)
 
-    # sampling
-    sampler_type = trial.suggest_categorical(
-        "sampler_type",
-        ["random_over", "random_under", "adasyn", "smote", "none"]
+    #Scaling
+    scaler=None
+    if SCALE_DATA:
+        scaler = StandardScaler()
+
     
-    )
-    if sampler_type == "random_over":
-        sampler = RandomOverSampler(random_state=random_seed)
-    elif sampler_type == "random_under":
-        sampler = RandomUnderSampler(random_state=random_seed)
-    elif sampler_type == "adasyn":
-        n_adasyn = trial.suggest_int("adasyn_n_neighs", 3, 20)
-        sampler = ADASYN(n_neighbors=n_adasyn, random_state=random_seed)
-    elif sampler_type == "smote":
-        n_smote = trial.suggest_int("smote_n_neighs", 3, 20)
-        sampler = SMOTE(k_neighbors=n_smote, random_state=random_seed)
-    else:
-        sampler = "passthrough"
+    #Sampling
+    sampler=None
+    if SAMPLE_DATA:
+        sampler_type = trial.suggest_categorical(
+            "sampler_type",
+            ["random_over", "random_under", "adasyn", "smote", "none"]
+        
+        )
+        if sampler_type == "random_over":
+            sampler = RandomOverSampler(random_state=SEED)
+        elif sampler_type == "random_under":
+            sampler = RandomUnderSampler(random_state=SEED)
+        elif sampler_type == "adasyn":
+            n_adasyn = trial.suggest_int("adasyn_n_neighs", 3, 10)
+            sampler = ADASYN(n_neighbors=n_adasyn, random_state=SEED)
+        elif sampler_type == "smote":
+            n_smote = trial.suggest_int("smote_n_neighs", 3, 10)
+            sampler = SMOTE(k_neighbors=n_smote, random_state=SEED)
+        else:
+            sampler = "passthrough"
 
-    # classifier
+    #Classifying
     classifier_type = trial.suggest_categorical(
         "classifier_type",
         ["rf", "xgb", "adab"]
     )
     if classifier_type == "rf":
-        depth = trial.suggest_int("rf_depth", 2, 32, log=True)
-        n_estimators = trial.suggest_int("rf_n", 100, 300)
-        clf = RandomForestClassifier(max_depth=depth, n_estimators=n_estimators, class_weight="balanced", random_state=42)
+        depth = trial.suggest_int("rf_depth", 2, 8, log=True)
+        n_estimators = trial.suggest_int("rf_n", 200, 500)
+        clf = RandomForestClassifier(max_depth=depth, n_estimators=n_estimators, class_weight="balanced", random_state=SEED)
 
     elif classifier_type == "xgb":
         param = {
-            "n_estimators": trial.suggest_int("xgb_n", 100, 300),
-            "max_depth": trial.suggest_int("xgb_d", 3, 9),
-            "learning_rate": trial.suggest_float("xgb_lr", 1e-3, 0.1, log=True),
-            "scale_pos_weight": trial.suggest_float("xgb_spw", 1.0, 20.0),
-            "random_state": 42
+            "n_estimators": trial.suggest_int("xgb_n", 300, 500),
+            "max_depth": trial.suggest_int("xgb_d", 3, 6),
+            "learning_rate": trial.suggest_float("xgb_lr", 1e-5, 0.1, log=True),#PROBAR 1e-5
+            "scale_pos_weight": trial.suggest_float("xgb_spw", 1.0, 5.0),
+            "random_state": SEED
         }
         clf = XGBClassifier(**param)
 
@@ -114,18 +135,23 @@ def objective(trial):
         base_depth = trial.suggest_int("adab_depth", 1, 3)
         clf = AdaBoostClassifier(
             estimator=DecisionTreeClassifier(max_depth=base_depth),
-            n_estimators=trial.suggest_int("adab_n", 50, 300),
-            learning_rate=trial.suggest_float("adab_lr", 0.01, 1.0, log=True),
-            random_state=42
+            n_estimators=trial.suggest_int("adab_n", 300, 700),
+            learning_rate=trial.suggest_float("adab_lr", 0.001, 1.0, log=True),
+            random_state=SEED
         )
 
-    scaler = StandardScaler()
-    pipeline = Pipeline(steps=[
-        ('imputer', imputer),
-        ('scaler', scaler),
-        ('sampler', sampler),
-        ('classifier', clf)
-    ])
+    #CREATE PIPELINE
+    pipe_arr=[]
+    if IMPUTE_DATA:
+        pipe_arr.append(('imputer',imputer))
+    if SCALE_DATA:
+        pipe_arr.append(('scaler',scaler))
+    if SAMPLE_DATA:
+        pipe_arr.append(('sampler',sampler))
+
+    pipe_arr.append(('classifier',clf))
+
+    pipeline = Pipeline(steps=pipe_arr)
 
     fold_scores = []
     for fold_idx, (train_idx, val_idx) in enumerate(splits):
@@ -135,7 +161,18 @@ def objective(trial):
         try:
             pipeline.fit(X_train, y_train)
             y_pred = pipeline.predict(X_val)
-            fold_score = f1_score(y_val, y_pred, average='macro')
+        
+            if METRIC=="f1":
+                fold_score = f1_score(y_val, y_pred, average='macro')
+
+            elif METRIC=="acc":
+                fold_score = accuracy_score(y_val, y_pred)
+            elif METRIC=="b_acc":
+                fold_score = balanced_accuracy_score(y_val, y_pred)
+            else:#F1
+                fold_score = f1_score(y_val, y_pred, average='macro')
+
+
         except Exception as e:
             print(f"Fold failed: {e}")
             return 0.0
@@ -152,18 +189,29 @@ def objective(trial):
 study = optuna.create_study(
     study_name="stroke_prediction_optimization",
     direction="maximize",
-    sampler=optuna.samplers.TPESampler(seed=random_seed, n_startup_trials=10),
+    sampler=optuna.samplers.TPESampler(seed=SEED, n_startup_trials=10),
     pruner=optuna.pruners.MedianPruner(n_startup_trials=10, n_warmup_steps=3)
 )
 
-study.optimize(objective, n_trials=100, show_progress_bar=True, n_jobs=-1)
+study.optimize(objective, n_trials=TRIAL_NUM, show_progress_bar=True, n_jobs=-1)
 
-print("\n--- Optimization Finished ---")
-print(f"Best F1-macro Score: {study.best_value:.4f}")
-print("Best Hyperparameters:")
+#PRINT AND SAVE
+lines = [
+    "\n--- Optimization Finished ---",
+    f"{TRIAL_NUM} trials",
+    f"Best Score: {study.best_value:.4f}",
+    "Best Hyperparameters:"
+]
 for key, value in study.best_params.items():
-    print(f"  {key}: {value}")
+    lines.append(f"  {key}: {value}")
 
 df_trials = study.trials_dataframe()
-print("\nBest sampler:", df_trials.groupby("params_sampler_type")["value"].mean().idxmax())
-print("Best classifier:", df_trials.groupby("params_classifier_type")["value"].mean().idxmax())
+lines.append(f"\nBest sampler: {df_trials.groupby('params_sampler_type')['value'].mean().idxmax()}")
+lines.append(f"Best classifier: {df_trials.groupby('params_classifier_type')['value'].mean().idxmax()}")
+lines.append("-" * 30)
+
+# Escribimos en ambos sitios
+with open(OUTPUT_FILE, "a") as f:
+    for line in lines:
+        print(line)       # Consola
+        print(line, file=f) # Archivo (modo append)
