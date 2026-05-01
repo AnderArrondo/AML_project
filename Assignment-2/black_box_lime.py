318070704f1a66d8eca1114492ffa1a4751068c1
Unai Lopez <u.lopez@opendeusto.es>
2026-05-01 12:23:14 +0200

    Black box models explained by LIME

---
+++ Assignment-2/black_box_lime.py
@@ -0,0 +1,131 @@
+import pandas as pd
+
+# sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import root_mean_squared_error, r2_score
+from xgboost import XGBRegressor
+
+# lime
+from interpret import show
+from interpret.blackbox import LimeTabular
+
+'''
+The objective of this script is to create three black-box ML models for
+explaining them locally using LIME in an interactive dashboard. 
+The black-box models to explain are a neural network, an 
+XGB and a random forest. 
+
+Data will be splitted into training and testing, without any subset for 
+validation in order to stay simple, avoiding model optimization, as the
+task is to understand why are the models taking decissions more than 
+improving the decissions themself.
+
+Only the first 250 predictions will be shown by the lime dashboard,
+but this parameter can be changed with the global variable SHOW_PREDS.
+Additionally models performance will also be shown.
+'''
+
+
+
+
+RANDOM_SEED = 42
+DATA_PATH = "Assignment-2/LengthOfStay.csv"
+SHOW_PREDS = 250 
+
+
+
+
+data = pd.read_csv(DATA_PATH) # .iloc[0:5000,:] for faster rendering
+
+# Data cleaning
+order=["0","1","2","3","4","5+"]
+data['rcount'] = pd.Categorical(data['rcount'], categories=order, ordered=True)
+
+categorical_cols = ['gender', 'rcount', 'facid']
+df = pd.get_dummies(data, columns=categorical_cols, drop_first=True)
+
+df = df.drop(columns=['eid', 'vdate', 'discharged'])
+
+
+
+# Data splitting
+X = df.drop(columns='lengthofstay')
+y = df['lengthofstay']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=RANDOM_SEED
+)
+
+
+
+# Models creation
+
+neural_network = make_pipeline(
+    StandardScaler(),
+    MLPRegressor(
+        hidden_layer_sizes=(15,15,15,15), # 3 hidden layers with 10 hidden units in each
+        activation='relu',
+        solver='adam',
+        max_iter=1000,
+        random_state=RANDOM_SEED,
+        early_stopping=True,
+        n_iter_no_change=50
+    )
+)
+
+random_forest = make_pipeline(
+    StandardScaler(),
+    RandomForestRegressor(
+        n_estimators=150,
+        random_state=RANDOM_SEED,
+    )
+)
+
+xgb = make_pipeline(
+    StandardScaler(),
+    XGBRegressor(
+        n_estimators=150,
+        random_state=RANDOM_SEED,
+        eval_metric='logloss'
+    )
+)
+
+models = {'nn': neural_network, 'rf': random_forest, 'xgb' :xgb}
+
+# Model training
+for model in models:
+    models[model].fit(X_train, y_train)
+
+
+# Model Results
+print("PERFORMANCE METRICS:\n")
+for model in models:
+    preds = models[model].predict(X_test)
+
+    print(f"{model}:\n\tRMSE: {root_mean_squared_error(y_test, preds)}\n\tR2: {r2_score(y_test, preds)}\n")
+
+
+
+# LIME
+for model in models:
+    print(f"Showing the model: {model}")
+
+    lime = LimeTabular(
+        model = models[model],
+        data=X_train,
+        random_state=RANDOM_SEED,
+        mode = 'regression'
+    )
+
+    lime_local = lime.explain_local(
+        X_test[:SHOW_PREDS],
+        y_test[:SHOW_PREDS],
+        name='lime'
+    )
+
+    show(lime_local)
+    input("")  # press enter to see the next model
